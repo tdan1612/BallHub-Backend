4ac37240c1df35684c5fd276e17165d60d4a4ff6
Gia Hien <ngogiahien2906@gmail.com>
2026-05-04 12:36:40 +0700

    Ep api tra ve mui gio viet nam

--- BallHub-BackEnd/src/main/java/com/ballhub/ballhub_backend/BallHubBackEndApplication.java
+++ BallHub-BackEnd/src/main/java/com/ballhub/ballhub_backend/BallHubBackEndApplication.java
@@ -1,12 +1,21 @@
 package com.ballhub.ballhub_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 public class BallHubBackEndApplication {
+
+	// ✅ ÉP MÚI GIỜ VIỆT NAM
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BallHubBackEndApplication.class, args);
